0e810da92f3a655b93c4173a53d2b25a6ed02ebf
yangyi <yangyi0907@mail.ustc.edu.cn>
2021-11-22 19:09:42 +0800

    1

--- src/leetcode/editor/cn/MaxChunksToMakeSorted.java
+++ src/leetcode/editor/cn/MaxChunksToMakeSorted.java
@@ -7,7 +7,7 @@ public class MaxChunksToMakeSorted {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-
+        return 0;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
